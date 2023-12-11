c28549ef77f98fb3644d0574a636b74d871b53b0
Laba Subagia <labasubagia22@gmail.com>
2023-12-11 11:31:18 +0800

    try update

--- sources/streamlit.py
+++ sources/streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from calc import add2
 
-st.title('Add 2 Val app')
+st.title('Add 2 Val app v1.0.0')
 
 n1 = st.number_input('Insert 1st number', step=1)
 n2 = st.number_input('Insert 2nd number', step=1)
